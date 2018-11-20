57c67268cfcee5bb8b2fc92fe2e60a6908be4756
aoruize <37258460+aoruize@users.noreply.github.com>
2018-11-19 20:43:47 -0500

    Add files via upload

---
+++ FLOPPY BIRD - FINAL/FloppyPenguinSettings.py
@@ -0,0 +1,18 @@
+# FloppyBirdSettings.py
+
+# game settings
+WIDTH, HEIGHT = 1280, 720
+CENTREX, CENTREY = WIDTH/2, HEIGHT/2
+WATER = HEIGHT - 580
+FPS = 60
+
+# colours
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREY = (170, 170, 170)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+LIGHT_BLUE = (140, 190, 255)
+GREY_BLUE = (90, 130, 170)
+PURPLE = (190, 95, 255)
